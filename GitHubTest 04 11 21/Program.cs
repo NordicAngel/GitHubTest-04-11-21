b3a432af136f109aa88d19bb6aee6dd1dee58381
NordicAngel <78414012+NordicAngel@users.noreply.github.com>
2021-11-04 10:04:57 +0100

    dca

--- GitHubTest 04 11 21/Program.cs
+++ GitHubTest 04 11 21/Program.cs
@@ -6,7 +6,7 @@ namespace GitHubTest_04_11_21
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to github!");
         }
     }
 }
